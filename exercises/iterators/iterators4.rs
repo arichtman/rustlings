7494108ab70e3d4765cb822be71cf920846f967a
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2024-09-20 12:39:30 +1000

    finish iterators

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,9 +3,9 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
+    // Damn I'm good
+    (1..=num).into_iter().fold(1, |v, c| c * v)
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
